820d8170a8fc6ec5cecfd4dd4ed3099f4b8af1ae
fdolzanes1 <fdolzanes1@gmail.com>
2019-11-17 15:34:46 -0400

    install mongoose e fazendo conexao com banco de dados

--- server.js
+++ server.js
@@ -1,5 +1,12 @@
 const express = require('express');
+const mongoose = require('mongoose');
+// Iniciando App
 const app = express();
+
+// Iniciando DB
+mongoose.connect('mongodb://localhost:27017/nodeapi', { useNewUrlParser: true });
+
+// Rota
 app.get('/', (req, res) => {
   res.send('Hello Rockeseat');
 });
