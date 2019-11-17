83d250cf208e14ddb0a52bce1b3ba9b676e27696
fdolzanes1 <fdolzanes1@gmail.com>
2019-11-17 15:54:06 -0400

    criando model de produto

--- server.js
+++ server.js
@@ -1,13 +1,23 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const requireDir = require('require-dir');
+
 // Iniciando App
 const app = express();
 
 // Iniciando DB
-mongoose.connect('mongodb://localhost:27017/nodeapi', { useNewUrlParser: true });
+mongoose.connect('mongodb://localhost:27017/nodeapi', {useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true });
+requireDir('./src/models');
+
+const Product = mongoose.model('Product');
 
 // Rota
 app.get('/', (req, res) => {
+  Product.create({
+    title: 'React', 
+    description: 'Build native apps', 
+    url: 'reactjs.io'
+  });
   res.send('Hello Rockeseat');
 });
 app.listen('3000');
