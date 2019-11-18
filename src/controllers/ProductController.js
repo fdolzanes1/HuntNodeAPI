de417fadfd3ba151a5cfe7c68cb42b87915943a8
fdolzanes1 <fdolzanes1@gmail.com>
2019-11-18 10:57:41 -0400

    add crud

--- src/controllers/ProductController.js
+++ src/controllers/ProductController.js
@@ -6,7 +6,19 @@ module.exports = {
     return res.json(products);
   },
   async store(req, res) {
-    const product = await Product.create(req.body);
-    return res.json(product);
+    const products = await Product.create(req.body);
+    return res.json(products);
+  }, 
+  async show(req, res) {
+    const products = await Product.findById(req.params.id);
+    return res.json(products);
+  },
+  async update(req, res){
+    const products = await Product.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    return res.json(products);
+  },
+  async destroy(req, res){
+    const products = await Product.findByIdAndDelete(req.params.id);
+    return res.json(products);
   }
 };
--- src/routes.js
+++ src/routes.js
@@ -4,5 +4,8 @@ const ProductControoler = require('./controllers/ProductController');
 
 routes.get('/products', ProductControoler.index);
 routes.post('/products', ProductControoler.store);
+routes.get('/products/:id', ProductControoler.show);
+routes.put('/products/:id', ProductControoler.update);
+routes.delete('/products/:id', ProductControoler.destroy);
 
 module.exports = routes;
