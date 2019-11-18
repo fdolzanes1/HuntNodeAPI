cea1e4a444d904e1d8128ad3127ade097de33d8f
fdolzanes1 <fdolzanes1@gmail.com>
2019-11-18 10:00:41 -0400

    add post no productcontroller

--- src/controllers/ProductController.js
+++ src/controllers/ProductController.js
@@ -4,5 +4,9 @@ module.exports = {
   async index(req, res) {
     const products = await Product.find();
     return res.json(products);
+  },
+  async store(req, res) {
+    const product = await Product.create(req.body);
+    return res.json(product);
   }
 };
--- src/routes.js
+++ src/routes.js
@@ -3,4 +3,6 @@ const routes = express.Router();
 const ProductControoler = require('./controllers/ProductController');
 
 routes.get('/products', ProductControoler.index);
+routes.post('/products', ProductControoler.store);
+
 module.exports = routes;
